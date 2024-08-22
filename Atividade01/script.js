bf5efd09bf5692ef1a633af55bd45a212b16ada1
leetobr <leetobr4@gmail.com>
2024-08-22 16:08:05 -0300

    SITE

---
+++ Atividade01/script.js
@@ -0,0 +1,6 @@
+function soma(){
+    var numero01 = document.getElementById("number1").value
+    var numero02 = document.getElementById("number2").value 
+    var resposta = document.getElementById("respSoma")
+    console.log(numero01,numero02,resposta)
+}
